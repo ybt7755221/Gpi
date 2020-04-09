161d7786365f442d9652c0ed9ec01ef42cef7f77
burt yu <ybt7755221@gmail.com>
2020-04-09 15:29:27 +0800

    开启sql debug 以及 增加首字母大小写操作

--- libraries/gutil/gutil.go
+++ libraries/gutil/gutil.go
@@ -16,55 +16,57 @@ func BeanUtil(out interface{}, in interface{}) {
 	outNum := outType.NumField()
 	for i := 0; i < outNum; i++ {
 		outFieldInfo := outType.Field(i)
-		inTypeInfo, _ := inType.FieldByName(outFieldInfo.Name)
-		outType := outFieldInfo.Type.String()
-		inType := inTypeInfo.Type.String()
-		inVal := inValue.FieldByName(outFieldInfo.Name)
-		if outType == inType {
-			outValue.FieldByName(outFieldInfo.Name).Set(reflect.Value(inVal))
-		} else {
-			var val interface{}
-			switch outType {
-			case "int":
-				if inType == "string" {
-					val, _ = strconv.Atoi(inVal.String())
-				} else {
-					val = int(inVal.Int())
+		inTypeInfo, ok := inType.FieldByName(outFieldInfo.Name)
+		if ok {
+			outType := outFieldInfo.Type.String()
+			inType := inTypeInfo.Type.String()
+			inVal := inValue.FieldByName(outFieldInfo.Name)
+			if outType == inType {
+				outValue.FieldByName(outFieldInfo.Name).Set(reflect.Value(inVal))
+			} else {
+				var val interface{}
+				switch outType {
+				case "int":
+					if inType == "string" {
+						val, _ = strconv.Atoi(inVal.String())
+					} else {
+						val = int(inVal.Int())
+					}
+				case "int32":
+					if inType == "string" {
+						val, _ = strconv.ParseInt(inVal.String(), 10, 32)
+					} else {
+						val = int32(inVal.Int())
+					}
+				case "int64":
+					if inType == "string" {
+						val, _ = strconv.ParseInt(inVal.String(), 10, 64)
+					} else {
+						val = int64(inVal.Int())
+					}
+				case "string":
+					if inType == "time.Time" {
+						val = inVal.Interface().(time.Time).Format("2006-01-02 15:04:05")
+					} else {
+						val = inVal.String()
+					}
+				case "float32":
+					val, _ = strconv.ParseFloat(inVal.String(), 32)
+				case "float64":
+					val, _ = strconv.ParseFloat(inVal.String(), 32)
+				case "time.Time":
+					tmpValue := inVal.String()
+					if len(tmpValue) == 10 && strings.Index(tmpValue, "-") == -1 {
+						intTm, _ := strconv.ParseInt(tmpValue, 10, 64)
+						tm := time.Unix(intTm, 0)
+						tmpValue = tm.Format("2006-01-02 15:04:05")
+					}
+					val, _ = time.Parse("2006-01-02 15:04:05", tmpValue)
+				default:
+					val = nil
 				}
-			case "int32":
-				if inType == "string" {
-					val, _ = strconv.ParseInt(inVal.String(), 10, 32)
-				} else {
-					val = int32(inVal.Int())
-				}
-			case "int64":
-				if inType == "string" {
-					val, _ = strconv.ParseInt(inVal.String(), 10, 64)
-				} else {
-					val = int64(inVal.Int())
-				}
-			case "string":
-				if inType == "time.Time" {
-					val = inVal.Interface().(time.Time).Format("2006-01-02 15:04:05")
-				} else {
-					val = inVal.String()
-				}
-			case "float32":
-				val, _ = strconv.ParseFloat(inVal.String(), 32)
-			case "float64":
-				val, _ = strconv.ParseFloat(inVal.String(), 32)
-			case "time.Time":
-				tmpValue := inVal.String()
-				if len(tmpValue) == 10 && strings.Index(tmpValue, "-") == -1 {
-					intTm, _ := strconv.ParseInt(tmpValue, 10, 64)
-					tm := time.Unix(intTm, 0)
-					tmpValue = tm.Format("2006-01-02 15:04:05")
-				}
-				val, _ = time.Parse("2006-01-02 15:04:05", tmpValue)
-			default:
-				val = nil
+				outValue.FieldByName(outFieldInfo.Name).Set(reflect.ValueOf(val))
 			}
-			outValue.FieldByName(outFieldInfo.Name).Set(reflect.ValueOf(val))
 		}
 	}
 }
